628e73fb2831247c018dc3f2e1d5734a5582e8a0
Rdrew123 <74625886+Rdrew123@users.noreply.github.com>
2025-09-14 01:26:29 +0200

    Update chart.py

--- strategy/chart.py
+++ strategy/chart.py
@@ -1,1 +1,21 @@
+import yfinance as yf
 
+PAIR_MAP = {
+    "USDJPY": "USDJPY=X",
+    "GBPUSD": "GBPUSD=X",
+    "XAUUSD": "XAUUSD=X",
+    "SILVER": "SILVER=X",
+    "EURUSD": "EURUSD=X",
+    "USDCAD": "USDCAD=X",
+    "CADCHF": "CADCHF=X",
+    "NZDCAD": "NZDCAD=X",
+    "USDNZD": "USDNZD=X",
+    "GBPJPY": "GBPJPY=X"
+}
+
+def get_live_data(pair, interval="1m", period="1d"):
+    df = yf.download(PAIR_MAP[pair], interval=interval, period=period)
+    df = df[['Open','High','Low','Close']]
+    df.columns = ['open','high','low','close']
+    df.reset_index(inplace=True)
+    return df
