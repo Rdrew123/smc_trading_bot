354799f8495334e9c8dcad0caaa5f64a08f63208
Rdrew123 <74625886+Rdrew123@users.noreply.github.com>
2025-09-14 01:34:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-from bot.notifier import send_trade_alert
+import time
+from bot.notifier import send_trade_alert_with_chart
+from strategy.chart import generate_chart
+from strategy.live_data import get_live_data
+from strategy.entries import detect_valid_entry_multi_tf, get_market_bias, TIMEFRAMES
 
-if __name__ == "__main__":
-    send_trade_alert("USDJPY", "149.50", "149.20", "149.80", "150.10", "150.50")
+PAIRS = ["USDJPY","GBPUSD","XAUUSD","SILVER","EURUSD","USDCAD","CADCHF","NZDCAD","USDNZD","GBPJPY"]
+
+while True:
+    for pair in PAIRS:
+        data_dict = {tf: get_live_data(pair, interval=tf) for tf in TIMEFRAMES}
+        market_bias = get_market_bias(data_dict)
+        entry_info = detect_valid_entry_multi_tf(data_dict, market_bias)
+        if entry_info:
+            chart_path = generate_chart(pair, data_dict["1m"])
+            send_trade_alert_with_chart(
+                pair,
+                entry_info['entry'],
+                entry_info['sl'],
+                entry_info['tp1'],
+                entry_info['tp2'],
+                entry_info['tp3'],
+                chart_path
+            )
+    time.sleep(60)
